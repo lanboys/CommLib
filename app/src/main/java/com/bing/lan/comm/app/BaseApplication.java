1f47857907f116f185429a107bfac6b083185eef
dsj-lanboys <lb@gdysj.com.cn>
2017-05-03 14:31:57 +0800

    update

--- app/src/main/java/com/bing/lan/comm/app/BaseApplication.java
+++ app/src/main/java/com/bing/lan/comm/app/BaseApplication.java
@@ -88,6 +88,7 @@ public class BaseApplication extends Application {
     public void onLowMemory() {
         super.onLowMemory();
         System.gc();
+        System.runFinalization();
     }
 
     //public boolean isNetworkAvailable = false;
